a2a4e2349acfb592153575417b5b1f6c45f75a86
rrc_f2023_Dongok Yang <dyang2@academic.rrc.ca>
2024-02-05 12:42:13 -0600

    practice project

---
+++ PracticeProject1/ConsoleApp1/Class1.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace practiceProject1
+{
+    internal class Rectangle
+    {
+        private double Width = 1;
+        private double Height = 1;
+        private double Area;
+        private double Perimeter;
+
+        public Rectangle()
+        {
+        }
+        public Rectangle(double width, double height)
+        {
+            Width = width;
+            Height = height;
+            Area = width * height;
+            Perimeter = (width + height) * 2;
+        }
+
+        public double GetWidth()
+        {
+            return Width;
+
+        }
+        public double GetHeight()
+        {
+            return Height;
+
+        }
+        public void SetWidth(double width)
+        {
+            Width = width;
+        }
+        public void SetHeight(double height)
+        {
+            Height = height;
+        }
+        public double GetArea(double width, double height)
+        {
+            Area = width * height;
+            return Area;
+        }
+        public double GetPerimeter(double width, double height)
+        {
+            Perimeter = (width + height) * 2;
+            return Perimeter;
+        }
+        public override string ToString()
+        {
+            return $"Rectangle - Width: {Width}, Height: {Height}, Area: {Area}, P:{Perimeter}";
+        }
+    }
+
+}
+
---
+++ PracticeProject1/ConsoleApp1/Program.cs
@@ -0,0 +1,43 @@
+﻿/*Project 1
+Create a Console App (.NET Framework) project.
+
+Declare and define a class named Rectangle to represent a rectangle.
+
+The class is define as:
+
+Two decimal attributes named width and height that specify the width and height of the Rectangle. 
+The default values are 1 for both width and height.
+A no-arg constructor that creates a default Rectangle.
+A constructor that creates a Rectangle with the specified width and height.
+A method named GetWidth() that returns the width of the Rectangle.
+A method named GetHeight() that returns the height of the Rectangle.
+A method named GetArea() that returns the area of the Rectangle.
+A method named GetPerimeter() that returns the perimeter of the Rectangle.
+A ToString() method that returns the string representation of the Rectangle in the following format:
+W: {width} H: {height} A: {area} P: {perimeter}
+
+Develop a console program that creates two Rectangle objects; one with a width of 4 and a height of 40. 
+The other with a width of 3.5 and a height of 35.9. Print the width, height, area, and perimeter of each Rectangle to the console.*/
+
+
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace practiceProject1
+
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Rectangle first = new Rectangle(4,40);
+            Rectangle second = new Rectangle(3.5, 35.9);
+            Console.WriteLine("First Rectangle: " + first.ToString());
+            Console.WriteLine("Second Rectangle: " + second.ToString());
+        }
+    }
+}
