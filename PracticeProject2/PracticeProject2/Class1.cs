f57402766560053af2ca0491b76a1a211a82ece8
rrc_f2023_Dongok Yang <dyang2@academic.rrc.ca>
2024-02-07 17:48:14 -0600

    practice project

---
+++ PracticeProject2/PracticeProject2/Class1.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PracticeProject2
+{
+    internal class Stock
+    {
+        private string symbol;
+        private string name;
+        private decimal previousClosingPrice;
+        private decimal currentPrice;
+        private decimal changedPercent;
+
+        public Stock(string symbol, string name, decimal previousClosingPrice)
+        {
+            this.symbol = symbol;
+            this.name = name;
+            this.previousClosingPrice = previousClosingPrice;
+            this.currentPrice = previousClosingPrice;
+        }
+        public decimal GetCurrentPrice()
+        {
+            return currentPrice;
+        }
+
+        public void SetCurrentPrice(decimal currentPrice)
+        {
+            this.currentPrice = currentPrice;
+        }
+
+        public decimal GetChangePercent()
+        {
+            changedPercent = (currentPrice - previousClosingPrice) / previousClosingPrice;
+            return changedPercent;
+        }
+    }
+}
