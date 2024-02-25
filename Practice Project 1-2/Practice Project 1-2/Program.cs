8a0bd7e21e409e6c7557620a289ca38a4b245090
rrc_f2023_Dongok Yang <dyang2@academic.rrc.ca>
2024-02-25 17:42:19 -0600

    get set methods

---
+++ Practice Project 1-2/Practice Project 1-2/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Practice_Project_1_2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Stock ORCL = new Stock("ORCL", "Oracle Corporation",34.5m);
+            ORCL.SetCurrentPrice(34.35m);
+            Console.WriteLine($"Price-Change Percentage: {ORCL.GetChangePercent():P}");
+        }
+    }
+}
---
+++ Practice Project 1-2/Practice Project 1-2/Stock.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Practice_Project_1_2
+{
+    internal class Stock
+    {
+        string symbol;
+        string name;
+        decimal previousClosingPrice;
+        decimal currentPrice;
+
+        public Stock(string symbol, string name, decimal currentPrice)
+        {
+            this.symbol = symbol;
+            this.name = name;
+            this.previousClosingPrice = currentPrice;
+            this.currentPrice = currentPrice;
+        }
+
+        public decimal GetCurrentPrice()
+        {
+            return currentPrice;
+        }
+        public void SetCurrentPrice(decimal newcurrentPrice)
+        {
+            previousClosingPrice = currentPrice;
+            currentPrice= newcurrentPrice;
+        }
+        public decimal GetChangePercent()
+        {
+            return (previousClosingPrice-currentPrice)/previousClosingPrice;
+        }
+    }
+}
