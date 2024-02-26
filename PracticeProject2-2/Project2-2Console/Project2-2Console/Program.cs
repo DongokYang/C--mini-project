f3d76a454bb1afe3f4acc7b7ac22ed73dc9c6956
rrc_f2023_Dongok Yang <dyang2@academic.rrc.ca>
2024-02-25 22:00:21 -0600

    auto-implemented declaring

---
+++ PracticeProject2-2/Project2-2Console/Project2-2Console/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Project2_2Library;
+namespace Project2_2Console
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Stock ORCL = new Stock("ORCL", "Oracle Corporation", 34.5m);
+            ORCL.CurrentPrice = 34.35m;
+            Console.WriteLine($"Price-Change Percentage: {ORCL.GetChangePercent():P}");
+        }
+    }
+}
---
+++ PracticeProject2-2/Project2-2Console/Project2-2Library/Stock.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Project2_2Library
+{
+    public class Stock
+    {
+        public string Symbol { get; private set; }
+        public string Name { get; private set; }
+        public decimal CurrentPrice { get; set; }
+        public decimal PreviousClosingPrice { get; private set; }
+        public Stock(string symbol, string name, decimal currentPrice)
+        {
+            Symbol = symbol; // Corrected assignment
+            Name = name;     // Corrected assignment
+            PreviousClosingPrice = currentPrice; // Corrected assignment
+            CurrentPrice = currentPrice;
+        }
+
+        public decimal GetChangePercent()
+        {
+            return (PreviousClosingPrice - CurrentPrice) / PreviousClosingPrice;
+        }
+    }
+}
